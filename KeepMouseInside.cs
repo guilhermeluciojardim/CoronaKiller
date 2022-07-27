3cd14f783c2b116d8919e9302f2a8f06d0418955
guilhermeluciojardim <guilhermelucio@gmail.com>
2022-07-27 13:54:38 -0300

    Update KeepMouseInside.cs

--- KeepMouseInside.cs
+++ KeepMouseInside.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class KeepMouseInside : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public PlayerController player;
 
     // Update is called once per frame
     void Update()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        if (player.gameOver == true){
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
